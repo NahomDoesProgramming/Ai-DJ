c3bef8319ac3f4ef9596c9a730af5a2dddee1162
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-03-24 18:34:59 -0700

    Add files via upload
    i had to wait a long time for the music to load

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-song1 = "";
-song2 = "";
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload()
 {
-    song1 = loadSound("music.mp3");
-    song2 = loadSound("music2.mp3");
+    song = loadSound("music.mp3");
+    song.setVolume(1);
+    song.rate(1);
+}
+function draw()
+{
+    image(video, 0, 0, 600, 500);
 }
 function setup()
 {
@@ -11,8 +19,27 @@ function setup()
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-function draw()
+function modelLoaded()
 {
-    image(video, 0, 0, 600, 500);
+    console.log("PoseNet is initialized!!");
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+}
+function play()
+{
+    song.play();
 }
